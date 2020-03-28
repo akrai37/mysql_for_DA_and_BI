3d7f99156850a5940e1226a72516c8c26b388530
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2020-03-28 20:41:42 +0530

    Add files via upload

---
+++ 2customers1table.sql
@@ -0,0 +1,11 @@
+SELECT * FROM sales.customers1;
+create table customers(
+ customer_id int , 
+ first_name varchar(255), 
+ last_name varchar(255), 
+ email_address varchar(255),  
+ number_of_complaints int
+ );
+ select * from sales_table;
+ select * from sales.sales_table;
+ 
