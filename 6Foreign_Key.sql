c169e6acf637737edb85be5fa7715de29ecf3622
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2020-03-29 23:37:13 +0530

    Update 6Foreign_Key.sql

--- 6Foreign_Key.sql
+++ 6Foreign_Key.sql
@@ -1,6 +1,7 @@
 use sales;
 alter table sales_table
-add foreign key (customer_id) references customers(customer_id) on delete cascade;
+add foreign key (customer_id) references customers(customer_id) on delete cascade;   #'on delete' is used for referencial deletion meaning if 
+#something has been deleted from the parent table, connected values (with foreign key) wil be deleted from the child table.
 
 alter table sales_table
 drop foreign key sales_table_ibfk_1;
