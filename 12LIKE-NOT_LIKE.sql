5dbf00a9d3be350422262690904b0e4300621f94
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2020-03-31 23:06:52 +0530

    Add files via upload

---
+++ 12LIKE-NOT_LIKE.sql
@@ -0,0 +1,9 @@
+select * from employees where first_name like ('Mark%');
+
+select * from employees where hire_date like '2000%';
+
+select * from employees where emp_no like ('1000_');
+
+select * from employees where first_name like ('%jack%');
+
+select * from employees where first_name not like ('%jack%');
