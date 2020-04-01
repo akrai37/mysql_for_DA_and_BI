d1da69866bd1cef5976d860099cec239c9209744
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2020-04-01 23:43:11 +0530

    Add files via upload

---
+++ 14(NOT)BETWEEN-AND.sql
@@ -0,0 +1,20 @@
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    salary BETWEEN 66000 AND 70000;
+
+SELECT 
+    *
+FROM
+    salaries
+WHERE
+    emp_no NOT BETWEEN 10004 AND 10012;
+
+SELECT 
+    *
+FROM
+    departments
+WHERE
+    dept_no BETWEEN 'd003' AND 'd006'; 
