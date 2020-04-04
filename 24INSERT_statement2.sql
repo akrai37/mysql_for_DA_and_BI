761b903e4d933ffac1f3f1f20e5a7c72cc85c52c
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2020-04-04 23:42:55 +0530

    Add files via upload

---
+++ 24INSERT_statement2.sql
@@ -0,0 +1,15 @@
+insert into departments(dept_no,dept_name)
+values(
+'d010','Business Analysis');
+
+select * from departments;
+
+# below code is to insert values from one table to another.
+
+CREATE TABLE departments_dup ( # creating a new table (which is supposed to be the duplicate of departments table)
+    dept_no CHAR(10),
+    dept_name VARCHAR(50)
+);
+
+insert into departments_dup(dept_no, dept_name) # inserting all the values of departments table into its duplicate table
+select * from departments;                         
