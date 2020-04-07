34d37324cbdf06f6b12beca1e363b0cd6a2556be
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2020-04-07 23:25:46 +0530

    Add files via upload

---
+++ 29MIN()_MAX().sql
@@ -0,0 +1,15 @@
+#MIN
+SELECT 
+    MIN(salary)
+FROM
+    salaries;
+    
+#MAX    
+SELECT 
+    MAX(salary)
+FROM
+    salaries;
+
+#query-2  MAX() MIN()    
+select max(emp_no) from employees;   
+select min(emp_no) from employees; 
